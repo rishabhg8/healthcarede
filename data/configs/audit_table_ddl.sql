5d3fc8f8f924c2de1848b1ec9e32adbcb8200d39
rishabhg8 <guptarishabh.3007@gmail.com>
2026-07-17 13:37:15 +0530

    bronze failed because of missing audit-log table, corrected it

--- data/configs/audit_table_ddl.sql
+++ data/configs/audit_table_ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE avd-databricks-demo.temp_dataset.audit_log (
+CREATE TABLE healthcarede-502306.temp_dataset.audit_log (
     data_source STRING,
     tablename STRING,
     load_type STRING,
